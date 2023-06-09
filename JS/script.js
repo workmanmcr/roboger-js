bc35ca98a62a6b65579c62890e174b6a1d27c0fb
workmanmcr <workmanmcr@gmail.com>
2023-06-09 10:22:00 -0700

    remove rentunboop function and create else if statement

--- JS/script.js
+++ JS/script.js
@@ -4,15 +4,12 @@ function returnBeep(number) {
   }
   return number;
 }
-function returnBoop(number) {
-  if (number.toString().includes('2')) {
-    return "boop";
-  }
-  return number;
-}
+
 function neighbor(number) {
   if (number.toString().includes('3')) {
     return "Won't you be my neighbor?";
-  }
+} else if (number.toString().includes('2')) {
+    return "boop";
   return number;
+}
 }
