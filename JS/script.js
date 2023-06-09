2979a887b078b31420dd6c10cd25186915b5600a
workmanmcr <workmanmcr@gmail.com>
2023-06-09 09:48:35 -0700

    run test on any number with 1 in it to return Beep

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,10 @@
 function returnBeep(number) {
+  if (number.toString().includes('1')) {
+    return "beep";
+  }
+  return number;
+}
+function returBoop(number) {
   if (number === 1) {
     return "beep";
   }
